a901f769f65de505ef9a1960db18e46f7896a571
darkwater <dark@dark.red>
2024-09-01 21:06:02 +0200

    feat: add layer shell apis

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:wayland_shell/wayland_shell.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await WaylandShell.init(
+    namespace: "wayland_shell_example",
+    layer: WaylandShellLayer.top,
+    anchors: [
+      WaylandShellEdge.top,
+      WaylandShellEdge.left,
+      WaylandShellEdge.right,
+    ],
+    exclusiveZone: 50,
+    height: 250,
+  );
+
   runApp(const MyApp());
 }
 
@@ -13,49 +27,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  @override
-  void initState() {
-    WaylandShell.clearInputRegions();
-
-    super.initState();
-  }
-
   bool e = true;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.transparent,
-        body: Center(
-          child: InputRegion(
-            enabled: e,
-            child: Container(
-              padding: const EdgeInsets.all(16.0),
-              color: Colors.white.withOpacity(0.2),
-              child: TextButton(
-                child: const Text(
-                  'Hello, World!',
-                  style: TextStyle(
-                    color: Colors.redAccent,
-                    fontSize: 32.0,
-                    // backgroundColor: Colors.greenAccent,
+        backgroundColor: Colors.black12,
+        body: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              height: 50,
+              color: Colors.blue,
+            ),
+            Expanded(
+              child: Center(
+                child: InputRegion(
+                  enabled: e,
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    color: Colors.white.withOpacity(e ? 0.2 : 0),
+                    child: TextButton(
+                      child: const Text(
+                        'Hello, World!',
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontSize: 24,
+                          // backgroundColor: Colors.greenAccent,
+                        ),
+                      ),
+                      onPressed: () async {
+                        print("Pressed");
+
+                        setState(() {
+                          e = false;
+                        });
+                        await Future.delayed(const Duration(seconds: 1));
+                        setState(() {
+                          e = true;
+
+                          print("Reset");
+                        });
+                      },
+                    ),
                   ),
                 ),
-                onPressed: () async {
-                  print("Pressed");
-          
-                  setState(() {
-                    e = false;
-                  });
-                  await Future.delayed(const Duration(seconds: 1));
-                  setState(() {
-                    e = true;
-                  });
-                },
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/src/wayland_shell_base.dart
+++ lib/src/wayland_shell_base.dart
@@ -1,7 +1,91 @@
 import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
 
 class WaylandShell {
   static const _methodChannel = MethodChannel('wayland_shell');
+
+  static Future<void> init({
+    required String namespace,
+    WaylandShellLayer layer = WaylandShellLayer.top,
+    List<WaylandShellEdge> anchors = const [],
+    EdgeInsets margins = EdgeInsets.zero,
+    // WaylandShellEdge? exclusiveEdge,
+    int exclusiveZone = 0,
+    WaylandShellKeyboardMode keyboardMode = WaylandShellKeyboardMode.none,
+    bool useInputRegions = false,
+    int? width,
+    int? height,
+  }) async {
+    if (width != null && height != null) {
+      await setSize(width, height);
+    } else if (width != null) {
+      await setWidth(width);
+    } else if (height != null) {
+      await setHeight(height);
+    }
+
+    await _methodChannel.invokeMethod('init', {
+      'namespace': namespace,
+      'layer': layer.index,
+      'anchors': anchors.map((e) => e.index).toList(),
+      'margins': [
+        margins.left.round(),
+        margins.right.round(),
+        margins.top.round(),
+        margins.bottom.round(),
+      ],
+      // 'exclusiveEdge': exclusiveEdge?.index,
+      'exclusiveZone': exclusiveZone,
+      'keyboardMode': keyboardMode.index,
+      'useInputRegions': useInputRegions,
+    });
+  }
+
+  static Future<void> setNamespace(String namespace) async {
+    await _methodChannel.invokeMethod('setNamespace', namespace);
+  }
+
+  static Future<void> setLayer(WaylandShellLayer layer) async {
+    await _methodChannel.invokeMethod('setLayer', layer.index);
+  }
+
+  static Future<void> setAnchor(WaylandShellEdge edge, bool enabled) async {
+    await _methodChannel.invokeMethod('setAnchor', [edge.index, enabled]);
+  }
+
+  static Future<void> setMargins(EdgeInsets margins) async {
+    await _methodChannel.invokeMethod('setMargins', [
+      margins.left.round(),
+      margins.right.round(),
+      margins.top.round(),
+      margins.bottom.round(),
+    ]);
+  }
+
+  // NOTE: blocked by https://github.com/wmww/gtk-layer-shell/pull/184
+  // static Future<void> setExclusiveEdge(WaylandShellEdge edge) async {
+  //   await _methodChannel.invokeMethod('setExclusiveEdge', edge.index);
+  // }
+
+  static Future<void> setExclusiveZone(int size) async {
+    await _methodChannel.invokeMethod('setExclusiveZone', size);
+  }
+
+  static Future<void> setKeyboardMode(WaylandShellKeyboardMode mode) async {
+    await _methodChannel.invokeMethod('setKeyboardMode', mode.index);
+  }
+
+  static Future<void> setSize(int width, int height) async {
+    await _methodChannel.invokeMethod('setSize', [width, height]);
+  }
+
+  static Future<void> setWidth(int width) async {
+    await _methodChannel.invokeMethod('setWidth', width);
+  }
+
+  static Future<void> setHeight(int height) async {
+    await _methodChannel.invokeMethod('setHeight', height);
+  }
 
   static Future<void> addInputRegion(int id, Rect rect) async {
     await _methodChannel.invokeMethod('addInputRegion', [
@@ -28,3 +112,25 @@ class WaylandShell {
   }
 }
 
+enum WaylandShellLayer {
+  // NOTE: order matches enum in gtk-layer-shell.h
+  background,
+  bottom,
+  top,
+  overlay,
+}
+
+enum WaylandShellEdge {
+  // NOTE: order matches enum in gtk-layer-shell.h
+  left,
+  right,
+  top,
+  bottom,
+}
+
+enum WaylandShellKeyboardMode {
+  // NOTE: order matches enum in gtk-layer-shell.h
+  none,
+  exclusive,
+  onDemand,
+}
